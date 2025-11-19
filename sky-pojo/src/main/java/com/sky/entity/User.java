4bef20023aeb70e118684c2d684995fe4e585abd
佳欣 李 <2150053680@qq.com>
2025-11-19 23:58:58 +0800

    导入分类管理功能模块

--- sky-pojo/src/main/java/com/sky/entity/User.java
+++ sky-pojo/src/main/java/com/sky/entity/User.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Tolerate;
 
 import java.io.Serializable;
 import java.time.LocalDate;
