ded61d3dee2aa9706a03c79b0e19ba3055e52323
佳欣 李 <2150053680@qq.com>
2025-11-24 23:59:34 +0800

    redis缓存菜品

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @Slf4j
 public class RedisConfiguration {
+
     @Bean
     public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
         log.info("开始创建RedisTemplate对象...");
